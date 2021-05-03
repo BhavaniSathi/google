96b538268bcd5908959b43b534072dda45627959
BhavaniSathi <69033132+BhavaniSathi@users.noreply.github.com>
2021-05-03 20:06:21 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+ 
+const app = express();
+ 
+app.get("/", function(req, res){
+    res.send("Welcome to Sweety's page");
+})
+app.get("/home", function(req, res){
+    res.send("Welcome to home page");
+})
+// Heroku will automatically set an environment variable called PORT
+const PORT = process.env.PORT || 3000;
+ 
+// Start the server
+app.listen(PORT, function(){
+    console.log("Server Starting running on http://localhost:"+PORT);
+})
